38f34099d713fc8977c60ad0cc95b43df9c51585
Uyennhi0997 <uyen.nhi0997@gmail.com>
2022-05-06 09:39:26 +0700

    merge origin/Tuan

--- co_cms/__manifest__.py
+++ co_cms/__manifest__.py
@@ -22,9 +22,6 @@
     'author': "CLOUDMEDIA CO.,LTD",
     'website': "https://cloudmedia.vn/",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
@@ -42,9 +39,6 @@
         'views/category_product_display_type_view.xml',
         'views/product_template_view.xml',
         'views/popups_view.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
     ],
     'application': True
 }
